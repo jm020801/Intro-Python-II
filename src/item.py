d6052bb1107097cc530a226e59512a00c26646cb
Jarrad Miller <jm020801@gmail.com>
2019-07-11 17:51:02 -0600

    error gone now fixing get and drop

--- src/item.py
+++ src/item.py
@@ -2,8 +2,6 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-    def __init__(self):
-        return self.name
     
     def on_take(self):
         print(f"You picked up the: {self.name}")
