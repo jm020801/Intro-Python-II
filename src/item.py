e17f3be5aa494330aa1e5af72e2d2a2d5b3b43f2
Jarrad Miller <jm020801@gmail.com>
2019-07-11 16:45:59 -0600

    items started. error persists

--- src/item.py
+++ src/item.py
@@ -2,3 +2,11 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+    def __init__(self):
+        return self.name
+    
+    def on_take(self):
+        print(f"You picked up the: {self.name}")
+
+    def on_drop(self):
+        print(f"You drop the: {self.name}")
--- src/player.py
+++ src/player.py
@@ -4,6 +4,7 @@ class Player:
     def __init__(self, name, starting_room):
         self.name = name
         self.current_room = starting_room
+        self.items = []
     def travel(self, direction):
         # Check if there's a valid room in the direction
         if getattr(self.current_room, f"{direction}_to") is not None:
@@ -12,4 +13,11 @@ class Player:
             print(self.current_room)
         else:
             # Else print an error message
-            print("Sorry! there's no room here.", "\n")
+            print("Sorry! There's no room this way...", "\n")
+    def _get_item_string(self):
+        if len(self.items) > 0:
+            return "\n" + ", ".join([item.name for item in self.items]) + "\n"
+        else:
+            return ""
+    def print_inventory(self):
+        print("You are carrying:\n " + ", ".join([item.name for item in self.items]) + "\n")
